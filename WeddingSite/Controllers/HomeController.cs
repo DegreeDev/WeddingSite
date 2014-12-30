a632c0e7f3e4380a488c9b0d43caf190389a2c6f
Matthew Lee <mslee61@gmail.com>
2014-12-30 17:50:05 -0600

    Initial Commit

---
+++ WeddingSite/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WeddingSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult RSVP()
+        {
+            return View();
+        }
+        public ActionResult Registry()
+        {
+            return View();
+        }
+        public ActionResult Reception()
+        {
+            return View();
+        }
+        public ActionResult Ceremony()
+        {
+            return View();
+        }
+        public ActionResult Photos()
+        {
+            return View();
+        }
+        public ActionResult Party()
+        {
+            return View();
+        }
+        public ActionResult Accommodations()
+        {
+            return View();
+        }
+
+    }
+}
